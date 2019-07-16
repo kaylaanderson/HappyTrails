2b1b3c85ee63683be814abd828dca52a75a8f7ad
Kayla Anderson <kaylanicolette@live.com>
2019-07-16 15:15:32 +0000

    Added geocoder & figaro gem

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,6 +1,8 @@
 class TrailsController < ApplicationController
 
   def index
+    request = 'get-trails?lat=40.027&maxResults=1&maxDistance=200&lon=-105.2519'
+    @content = call_web_api(request)
   end
   
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,4 @@
 class Trail < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode
 end
