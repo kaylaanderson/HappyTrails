cde5a2b40e521bc9193b9f8cb31c5a77aa77723c
Kayla Anderson <kaylanicolette@live.com>
2019-08-05 23:35:35 +0000

    RSpec test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'trails#index'
-  resources :trails
+  root "trails#index"
+  resources :trails, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/controllers/trails_controller_spec.rb
+++ spec/controllers/trails_controller_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe TrailsController, type: :controller do 
-  describe "trails_pages#index" do 
+  describe "trails#index action" do 
     it "should display the expected data" do 
       get :index 
-      expect(response).to have_http_status :success
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "trails#show action" do 
+    it "should successfully show the trails details page" do 
+      get :show
+      expect(response).to have_http_status(:success)
     end
   end
 end
